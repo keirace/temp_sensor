bc74a1c2ad85a9196af309221b736a05b707976b
Pinkaew <6010613401@student.tu.ac.th>
2021-01-03 09:24:30 +0700

    code updated

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from threading import Thread
 import paho.mqtt.client as mqtt
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 
 
 app = Flask(__name__, static_url_path="/static")
@@ -27,16 +28,15 @@ NGROK_TOKEN = os.environ["NGROK_TOKEN"]
 pyngrok_config = PyngrokConfig(region="jp")
 ngrok.set_auth_token(NGROK_TOKEN)
 
-setting = False
-notify = False
-userID = None
-client = None
-inp = None
-graph = False
+setting = notify = graph = False
+userID = client = typ = inp = None
+current = max_temp = min_temp = -1
+time = 60
 topic = "@msg/settings"
 ls = []
-max_temp = -1
-min_temp = -1
+
+carousel = json.loads(open("flex.json", 'rb').read().decode('utf8'))["carousel"]
+flex = json.loads(open("flex.json", 'rb').read().decode('utf8'))["notification"]
 
 @app.route("/callback", methods=["POST"])
 def callback():
@@ -60,15 +60,16 @@ def callback():
 @handler.add(FollowEvent)
 def follow(event):
     profile = line_bot_api.get_profile(event.source.user_id)
+    global carousel
     line_bot_api.push_message(
         event.source.user_id,
         [
-            TextSendMessage("Hello, @%s this is LINE Temp Sensor" % profile.display_name),
-            StickerSendMessage(package_id="11539", sticker_id="52114131")
+            TextSendMessage("Hello, %s this is LINE TempSensor" % profile.display_name),
+            StickerSendMessage(package_id="11539", sticker_id="52114131"),
+            FlexSendMessage(alt_text="Options", contents=carousel)
         ]
-        
     )
-    line_bot_api.link_rich_menu_to_user(event.source.user_id, "richmenu-af4d07b32f2eeda666317224566eaa13")
+    line_bot_api.link_rich_menu_to_user('richmenu-12845d8500ffca1300a28355bd3f4580')
 
 ####################################################################################
 #   Message Handler
@@ -79,46 +80,87 @@ def message_text(event):
     if isinstance(event.source, SourceUser):
         global setting, notify, userID, ls, endpoint, graph
         msg = event.message.text
-        if msg == "graph":
-            publish('graph')
-            graph = True
+        userID = event.source.user_id
+        if msg == "menus":
+            global carousel
             line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text="We are processing your temperature graph for the period of 30 seconds. Please wait...")
+                event.reply_token,
+                FlexSendMessage(alt_text="Menus", contents=carousel)
             )
-            
+
+        if msg == "temp now":
+            publish(msg)
+            global current
+            while current == -1:
+                pass
+            fahrenheit = str(round(1.8*float(current) + 32, 2))
+            kelvin = str(round(float(current)+273.15, 2))
+            global flex
+            edit_flex("Current", f"{current} °c, {fahrenheit} °f, {kelvin} K")
+            line_bot_api.push_message(
+                userID,
+                FlexSendMessage(alt_text="Current", contents=flex)
+            )
+            current = -1
+        if msg == "graph":
+            length = json.loads(open("flex.json", 'rb').read().decode('utf8'))["length"]
+            line_bot_api.reply_message(
+                event.reply_token,
+                FlexSendMessage("Graph duration", length)
+            )
+            graph = True
+        if graph:
+            if msg.isnumeric() and int(msg) <= 60:
+                global time 
+                time = int(msg)
+                publish('graph')
+                if len(ls) < time:
+                    line_bot_api.reply_message(
+                        event.reply_token, 
+                        TextSendMessage(text="We are processing your temperature graph for the next period of %d seconds. Please wait..."%(time-len(ls)))
+                    )
+                    g = Thread(target=graph_creator, args=[True], daemon=True)
+                    g.start()
+                else:
+                    graph_creator(False)
+                    new_url = endpoint + '/static/graph.png'
+                    line_bot_api.reply_message(
+                        event.reply_token, 
+                        ImageSendMessage(new_url, new_url)
+                    )
+                    graph = False
+            else: 
+                line_bot_api.reply_message(event.reply_token, "Graph canceled, please provide number (max:60)")
+                graph=False
         if msg == "notify me" and not notify:
             line_bot_api.reply_message(
                 event.reply_token, TextSendMessage(text="Notify every how many sec?")
             )
             setting = True
             publish("yes")
-            userID = event.source.user_id
             line_bot_api.link_rich_menu_to_user(event.source.user_id, "richmenu-a692a8fcd00f8db98fe111294535c762")
+
         elif msg == "notify me" and notify:
-            line_bot_api.reply_message(
-                event.reply_token,
-                [
-                    TextSendMessage(text="You have already turned on the temperature notification."),
-                    TextSendMessage(text="To change notification time please turn off the notification first.")
-                ]
-            )
-        if msg == "cancel" and notify:
+            global min_temp, max_temp
             publish("no")
             setting = False
             notify = False
+            while min_temp == -1 or max_temp == -1:
+                pass
+            edit_flex("Report", f"Max: {max_temp} °c, Min: {min_temp} °c")
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text="Notification canceled.")
+                [
+                    TextSendMessage(text="Notification canceled."),
+                    FlexSendMessage(alt_text="Report", contents=flex)
+                ]
             )
-        elif msg == "cancel" and not notify:
-            line_bot_api.reply_message(
-                event.reply_token,
-                    TextSendMessage(text="You have already turned off the notification.")
-            )
-        if setting and msg != "notify me":
-            try:
+            min_temp, max_temp = -1, -1
+
+        if setting and msg != 'notify me':
+            if msg.isnumeric():
                 sec = int(msg)
-                if sec >= 1 and sec <= 60:
+                if sec >= 1:
                     setting = False
                     notify = True
                     publish(sec)
@@ -128,25 +170,58 @@ def message_text(event):
                         event.reply_token,
                         TextSendMessage(text="Your preference has been set.")
                     )
-                else:
-                    line_bot_api.reply_message(
-                        event.reply_token,
-                        TextSendMessage(text="Please provide number between 1-60.")
-                    )
-            except:
-                print("in exception")
+            else:
                 line_bot_api.reply_message(
                     event.reply_token,
-                    TextSendMessage(text="Please provide number between 1-60.")
-                    )
-    
+                    TextSendMessage(text="Canceled. Please provide number. (min:1)")
+                )
+                setting = False
+                line_bot_api.unlink_rich_menu_from_user(event.source.user_id)
+            
+
+####################################################################################
+#   Graph, Flex and Notification
+####################################################################################
+
+def graph_creator(wait):
+    global ls, typ
+    y_pos = [i for i in range(1, time+1)]
+    while len(ls) < time:
+        pass
+    plt.style.use('ggplot')
+    if len(ls) > time:
+        new_ls = ls[60-time:60]
+        plt.bar(y_pos, new_ls, color=(0.4, 0.5, 0.6, 0.6))
+    else:
+        plt.bar(y_pos, ls, color=(0.4, 0.5, 0.6, 0.6))
+    plt.savefig("static/graph.png")
+    publish('fin_graph')
+    new_url = endpoint + '/static/graph.png'
+    if wait:
+        line_bot_api.push_message(
+            userID,
+            ImageSendMessage(new_url, new_url)
+        )
+
+def edit_flex(header, body):
+    global flex
+    flex['header']['contents'][0]['text'] = header
+    flex['body']['contents'][0]['text'] = body
+
 def notification(typ, temp):
     global notify, userID
-    fahrenheit = 1.8*float(temp) + 32
-    kelvin = float(temp)+273.15
+    header = ""
+    if typ == 'Max' or typ == 'Min':
+        header = "New " + typ
+    else: 
+        header = typ
+    fahrenheit = str(round(1.8*float(temp) + 32, 2))
+    kelvin = str(round(float(temp)+273.15, 2))
+    global flex
+    edit_flex(header, f"{temp} °c, {fahrenheit} °f, {kelvin} K")
     line_bot_api.push_message(
-            userID,
-            TextSendMessage(f"{typ} : \n{temp} °c\n{round(fahrenheit, 2)} °f\n{round(kelvin, 2)} K")
+        userID,
+        FlexSendMessage(alt_text=typ, contents=flex)
     )
 
 ####################################################################################
@@ -165,42 +240,30 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("@msg/tmp")
 
 def get_message():
+    global inp
     buff = str(inp.split(":")).split('\'')
     return buff[1], buff[3]
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    global inp, graph, ls, userID, max_temp, min_temp
+    global ls, userID, max_temp, min_temp, typ, inp
     inp = msg.payload.decode("utf-8")
     typ, temp = get_message()
-    if notify:
+    if typ == "Current" or typ == "Temp":
+        global current
+        current = temp
+    if notify or typ=='Rec':
         notification(typ, temp)
-    if graph:
-        y_pos = [i for i in range(1, 31)]
-        if typ == 'Temp' and len(ls) < 30:
-            ls.append(temp)
-        if len(ls) == 30:
-            plt.bar(y_pos, ls, color=(0.2, 0.4, 0.6, 0.6))
-            plt.savefig("static/graph.png")
-            ls = []
-            publish('fin_graph')
-            new_url = endpoint + '/static/graph.png'
-            line_bot_api.push_message(
-                userID,
-                ImageSendMessage(new_url, new_url)
-            )
-            graph = False
     if typ == 'ReportMax' or typ == 'ReportMin':
         if typ == 'ReportMax':
             max_temp = temp
         elif typ == 'ReportMin':
             min_temp = temp
-        if min_temp != -1 and max_temp != -1:
-            line_bot_api.push_message(
-                userID,
-                TextSendMessage(text=f"Report:\nMax: {max_temp}°c, Min: {min_temp}°c")
-            )
-            min_temp, max_temp = -1, -1
+    if typ == 'Temp':
+        if len(ls) == time:
+            ls.pop(0)
+        ls.append(float(temp))
+    
 
 def publish(msg):
     result = client.publish(topic, msg)
@@ -234,9 +297,8 @@ if __name__ == "__main__":
     endpoint = endpoint.replace("http://", "https://")
     print(endpoint)
     line_bot_api.set_webhook_endpoint(endpoint + "/callback")
-    # m5.run()
     t = Thread(target=run)
     t.daemon = True
     t.start()
-    line_bot_api.set_default_rich_menu("richmenu-af4d07b32f2eeda666317224566eaa13")
+    line_bot_api.set_default_rich_menu("richmenu-12845d8500ffca1300a28355bd3f4580")
     app.run(port=5001, use_reloader=False)
